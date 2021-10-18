f5dee324554259b13dd94432247221e15863e574
brandonlokesy <73189793+brandonlokesy@users.noreply.github.com>
2021-10-18 21:00:07 +0800

    update

---
+++ esp3902_motor/esp3902_motor.ino
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTY AND SUPPORT
+ * IS APPLICABLE TO THIS SOFTWARE IN ANY FORM. CYTRON TECHNOLOGIES SHALL NOT,
+ * IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR CONSEQUENTIAL
+ * DAMAGES, FOR ANY REASON WHATSOEVER.
+ ********************************************************************************
+ * DESCRIPTION:
+ *
+ * This example shows how to drive 2 motors using the PWM and DIR pins with
+ * 2-channel motor driver.
+ * 
+ * 
+ * CONNECTIONS:
+ * 
+ * Arduino D3  - Motor Driver PWM 1 Input
+ * Arduino D4  - Motor Driver DIR 1 Input
+ * Arduino D9  - Motor Driver PWM 2 Input
+ * Arduino D10 - Motor Driver DIR 2 Input
+ * Arduino GND - Motor Driver GND
+ *
+ *
+ * AUTHOR   : Kong Wai Weng
+ * COMPANY  : Cytron Technologies Sdn Bhd
+ * WEBSITE  : www.cytron.io
+ * EMAIL    : support@cytron.io
+ *
+ *******************************************************************************/
+
+ #include <CytronMotorDriver.h>
+
+
+// Configure the motor driver.
+CytronMD motor1(PWM_DIR, 3, 4);  // PWM 1 = Pin 3, DIR 1 = Pin 4.
+CytronMD motor2(PWM_DIR, 11, 12); // PWM 2 = Pin 9, DIR 2 = Pin 10.
+int incomingData;
+
+// The setup routine runs once when you press reset.
+void setup() {
+  Serial.begin(9600);
+}
+
+
+// The loop routine runs over and over again forever.
+void loop() {
+  if (Serial.available() > 0){
+    incomingData = Serial.read();
+
+    if (incomingData == 'F'){
+      motor1.setSpeed(200);
+    }
+
+    if (incomingData == 'Q'){
+      motor1.setSpeed(0);
+    }
+    if (incomingData == 'V'){
+      motor2.setSpeed(255);
+    }
+
+    if (incomingData == 'S'){
+      motor2.setSpeed(0);
+    }
+  }
+}
---
+++ proof2.py
@@ -0,0 +1,16 @@
+import proof
+# from legobrick import Lego
+
+# lego = Lego()
+
+class A(object):
+    def __init__(self):
+        self.property = None
+
+    def update(self):
+        print(lego.get_xy())
+
+a = A()
+
+# print(a.property)
+a.update()
